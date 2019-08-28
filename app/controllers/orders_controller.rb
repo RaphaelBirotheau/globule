87aa09ff0a8276eabb285fd84ded4693c85d7ddf
raphael birotheau <raphael.birotheau@gmail.com>
2019-08-28 18:39:02 +0200

    ON PEUT CREER DES CODES BARRE

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,24 @@
 class OrdersController < ApplicationController
+
   def last
   end
 
   def recommended
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    shopping_list = order_params[:shopping_list]
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:shopping_list)
   end
 end
