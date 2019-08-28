8d07b2c66377385eba48f35ee1da22d1cef53f80
raphael birotheau <raphael.birotheau@gmail.com>
2019-08-28 11:52:39 +0200

    added user icon and routes (ITS NOT DONE YET PLEASE DONT COMPLAIN

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,78 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_122310) do
+ActiveRecord::Schema.define(version: 2019_08_27_091415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_items", force: :cascade do |t|
+    t.string "discount"
+    t.integer "quantity"
+    t.integer "price"
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "purchase_date"
+    t.string "nutrition_score_repartition"
+    t.string "nutrition_repartition"
+    t.string "nova_repartition"
+    t.string "label_repartition"
+    t.string "packaging_repartition"
+    t.string "product_category_repartition"
+    t.string "product_sub_category_repartition"
+    t.string "country_origin_repartion"
+    t.string "additives_repartition"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "nutrition_grades_tags"
+    t.string "nova_group"
+    t.string "allergens_tags"
+    t.string "generic_name_fr"
+    t.string "image_front_url"
+    t.string "code"
+    t.string "categories_hierarchy"
+    t.string "additives_tags"
+    t.string "brands"
+    t.string "stores_tags"
+    t.string "labels_tags"
+    t.string "countries_tags"
+    t.string "ingredients_tags"
+    t.string "packaging_tags"
+    t.string "product_quantity"
+    t.string "serving_quantity"
+    t.string "ingredients_analysis_tags"
+    t.string "nutriments"
+    t.string "pnns_group_1"
+    t.string "pnns_group_2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_profiles", force: :cascade do |t|
+    t.string "nutrition_score_repartition"
+    t.string "nutrition_repartition"
+    t.string "nova_repartition"
+    t.string "label_repartition"
+    t.string "packaging_repartition"
+    t.string "product_category_repartition"
+    t.string "product_sub_category_repartition"
+    t.string "country_origin_repartion"
+    t.string "additives_repartition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +95,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_122310) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "orders", "users"
 end
