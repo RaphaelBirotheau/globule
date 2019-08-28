7beef54c46a95c9bc8bbac5552e57fd004774b69
Edouard Blondeau <edouardbl@gmail.com>
2019-08-28 18:11:20 +0200

    started the homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_08_28_120927) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "baskets", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "order_items", force: :cascade do |t|
     t.string "discount"
