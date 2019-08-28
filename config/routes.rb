35bb9a2097761eec78ff8d872c450495fada0218
raphael birotheau <raphael.birotheau@gmail.com>
2019-08-28 12:53:34 +0200

    added path for profile and basket soon

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   resources :products, only: [:show]
 
+  resources :baskets, only:[:show]
+
   root to: 'pages#home'
+
+  get "/my_profile", to: "users#profile"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
