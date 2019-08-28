2df1936e17b59d1f90949f7484b900729edd3836
raphael birotheau <raphael.birotheau@gmail.com>
2019-08-28 15:14:39 +0200

    added dashboard button

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get "/recommandations", to: "orders#recommended"
   get "/lastbasket", to: "orders#last"
   get "/profile", to: "profiles#show"
+  get "/dashboard", to: "profiles#dashboard"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
