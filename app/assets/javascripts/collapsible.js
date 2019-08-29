40b1445da16e8141f926f99d566619f50c14f5f8
raphael birotheau <raphael.birotheau@gmail.com>
2019-08-29 12:13:58 +0200

    added collapse java & bootstrap

---
+++ app/assets/javascripts/collapsible.js
@@ -0,0 +1,21 @@
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.maxHeight){
+//       content.style.maxHeight = null;
+//     } else {
+//       content.style.maxHeight = content.scrollHeight + "px";
+//     }
+//   });
+// }
+
+A METTRE DANS LE HTML
+//  <button class="collapsible"><p><%= product.generic_name_fr %></p></button>
+// <div class="content">
+//   <p><%= product.nutrition_grades_tags %></p>
+// </div>
+// <% end %>
