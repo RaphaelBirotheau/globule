f203002fe4ee4096af4004b70917f6a25cea113d
thomas bozzo <thomas.bozzo@me.com>
2019-08-30 14:47:33 +0200

    navbar modified

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 2019_08_29_141824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "baskets", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "order_items", force: :cascade do |t|
     t.string "discount"
     t.integer "quantity"
