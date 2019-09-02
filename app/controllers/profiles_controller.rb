6a5d7ede7620b8ee6cbe9dc69ba525cc978c39af
Edouard Blondeau <edouardbl@gmail.com>
2019-09-02 18:25:41 +0200

    added a dashboard with Nova and Nutri Score

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,21 @@ class ProfilesController < ApplicationController
   end
 
   def dashboard
+    nutrition_score
+    nova_score
   end
 
+  private
+
+  def nutrition_score
+    scores = current_user.orders.pluck(:nutrition_score_repartition).map { |score| JSON.parse(score) }.flatten
+    scores = scores.inject(Hash.new(0)) { |total, e| total[e] += 1 ;total }
+    @nutriscores = Hash[scores.to_a.sort_by { |key, val| key }]
+  end
+
+  def nova_score
+    scores = current_user.orders.pluck(:nova_repartition).map { |score| JSON.parse(score) }.flatten
+    scores = scores.inject(Hash.new(0)) { |total, e| total[e] += 1 ;total }
+    @novascores = Hash[scores.to_a.sort_by { |key, val| key }]
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,6 @@
 import "bootstrap";
 
-// import "chartkick";
-// import "chart.js";
-
-import Chartkick from "chartkick";
-import Chart from "chart.js";
-
 import linkTrigger from '../components/linkTrigger';
-
-Chartkick.use(Chart);
 
 linkTrigger();
 
