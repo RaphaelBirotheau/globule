1cff34e207f621e25e54953ad80f7cb7cf91bdf9
Felix Letellier <felix.letellier.merida@gmail.com>
2019-09-03 10:07:35 +0200

    First iteration of our recommendation algo

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,8 @@ class Product < ApplicationRecord
   validates :code, presence: true
 
   before_create :call_open_food_fact
-  after_create :create_pnns
+  after_create :create_pnns, if: :code_not_nil?
+  after_create :total_score_compute
   # after_create :scoring
 
   def call_open_food_fact
@@ -19,27 +20,30 @@ class Product < ApplicationRecord
     url = "https://fr.openfoodfacts.org/api/v0/produit/#{barcode}.json"
     product_serialized = open(url).read
     product = JSON.parse(product_serialized)
-    if product["status_verbose"] != "product not found"
-      self.nutrition_grades_tags = product["product"]["nutrition_grades_tags"][0]
-      self.nova_group = product["product"]["nova_group"]
-      self.allergens_tags = product["product"]["allergens_tags"]
-      self.generic_name_fr = product["product"]["product_name"]
-      self.categories_hierarchy = product["product"]["categories_hierarchy"].select { |category| category.start_with?('en:') }[-1]
-
-      self.additives_tags = product["product"]["additives_tags"]
-      self.brands = product["product"]["brands"]
-      self.stores_tags = product["product"]["stores_tags"]
-      self.labels_tags = product["product"]["labels_tags"]
-      self.countries_tags = product["product"]["countries"]
-      self.ingredients_tags = product["product"]["ingredients_tags"]
-      self.packaging_tags = product["product"]["packaging_tags"]
-      self.product_quantity = product["product"]["product_quantity"]
-      self.serving_quantity = product["product"]["serving_quantity"]
-      self.ingredients_analysis_tags = product["product"]["ingredients_analysis_tags"]
-      self.nutriments = product["product"]["nutriments"]
-      self.pnns_group_1 = product["product"]["pnns_groups_1"]
-      self.pnns_group_2 = product["product"]["pnns_groups_2"]
-      self.image_front_url = product["product"]["image_front_url"]
+    if (product["status_verbose"] != "product not found")
+      if  (PnnsSecondGroup.distinct.pluck(:name).exclude?(product["product"]["pnns_groups_2"]))
+        self.code = nil
+      else
+        self.nutrition_grades_tags = product["product"]["nutrition_grades_tags"][0]
+        self.nova_group = product["product"]["nova_group"]
+        self.allergens_tags = product["product"]["allergens_tags"]
+        self.generic_name_fr = product["product"]["product_name"]
+        self.categories_hierarchy = product["product"]["categories_hierarchy"].select { |category| category.start_with?('en:') }
+        self.additives_tags = product["product"]["additives_tags"]
+        self.brands = product["product"]["brands"]
+        self.stores_tags = product["product"]["stores_tags"]
+        self.labels_tags = product["product"]["labels_tags"]
+        self.countries_tags = product["product"]["origins_tags"]
+        self.ingredients_tags = product["product"]["ingredients_tags"]
+        self.packaging_tags = product["product"]["packaging_tags"]
+        self.product_quantity = product["product"]["product_quantity"]
+        self.serving_quantity = product["product"]["serving_quantity"]
+        self.ingredients_analysis_tags = product["product"]["ingredients_analysis_tags"]
+        self.nutriments = product["product"]["nutriments"]
+        self.pnns_group_1 = product["product"]["pnns_groups_1"]
+        self.pnns_group_2 = product["product"]["pnns_groups_2"]
+        self.image_front_url = product["product"]["image_front_url"]
+      end
     #   self.save
     else
       self.code = nil
@@ -49,58 +53,71 @@ class Product < ApplicationRecord
     #
   end
 
+  def code_not_nil?
+    self.code != nil
+  end
+
   def create_pnns
-    PnnsProduct.create!(product: self, pnns_second_group: PnnsSecondGroup.find_by(name: self.pnns_group_2))
+    # if self.code != nil
+      PnnsProduct.create!(product: self, pnns_second_group: PnnsSecondGroup.find_by(name: self.pnns_group_2))
+    # end
   end
 
   def health_score
-
-    if has_labels?(JSON.parse(self.labels_tags)) && (JSON.parse(self.additives_tags).any?) && has_plastic?(JSON.parse(self.packaging_tags))
-      pnns_second_group.health_score + 6
-    elsif has_labels?(JSON.parse(self.labels_tags)) && (JSON.parse(self.additives_tags).any?)
-      pnns_second_group.health_score + 4
-    elsif has_labels?(JSON.parse(self.labels_tags))
-      pnns_second_group.health_score + 2
+    if self.pnns_group_2 == "unknown"
+      nutri_score(self.nutrition_grades_tags) + nova_score(self.nova_group)
+    elsif has_labels_bio?(JSON.parse(self.labels_tags))
+      nutri_score(self.nutrition_grades_tags) + nova_score(self.nova_group) + additive_score(JSON.parse(self.additives_tags)) + 10
     else
-      pnns_second_group.health_score
+      nutri_score(self.nutrition_grades_tags) + nova_score(self.nova_group) + additive_score(JSON.parse(self.additives_tags))
     end
   end
 
-  def environmental_score
-    if has_labels?(JSON.parse(self.labels_tags)) && is_french?(self.countries_tags) && has_plastic?(self.packaging_tags)
-      pnns_second_group.environmental_score + 6
-    elsif has_labels?(JSON.parse(self.labels_tags)) && is_french?(self.countries_tags)
-      pnns_second_group.environmental_score + 4
-    elsif has_labels?(JSON.parse(self.labels_tags))
-      pnns_second_group.environmental_score + 2
+  def compute_environmental_score
+    if has_labels_bio?(JSON.parse(self.labels_tags)) && is_french?(self.countries_tags) && has_plastic?(self.packaging_tags)
+      self.pnns_product.pnns_second_group.environmental_score + (nova_score(self.nova_group)*2) + 50
+    elsif has_labels_bio?(JSON.parse(self.labels_tags)) && is_french?(self.countries_tags)
+      self.pnns_product.pnns_second_group.environmental_score + (nova_score(self.nova_group)*2) + 40
+    elsif has_labels_bio?(JSON.parse(self.labels_tags))
+      self.pnns_product.pnns_second_group.environmental_score + (nova_score(self.nova_group)*2) + 20
     else
-      pnns_second_group.environmental_score
+      2
     end
   end
 
   def social_score
-    if self.countries_tags.downcase.include?("france")
-      2
-    elsif (self.countries_tags.downcase.include?("france")) && (JSON.parse(self.labels_tags).any? {|label| label.downcase.include? "bio" || "equitable" || "organic" || "fairtrade"})
-      4
+    if has_labels_bio?(JSON.parse(self.labels_tags))
+      50
+    elsif has_labels_bio?(JSON.parse(self.labels_tags)) && has_labels_fairtrade?(JSON.parse(self.labels_tags))
+      75
+    elsif has_labels_bio?(JSON.parse(self.labels_tags)) && has_labels_fairtrade?(JSON.parse(self.labels_tags)) && is_french?(self.countries_tags)
+      100
+    elsif is_french?(self.countries_tags)
+      25
     else
-      0
+    0
     end
   end
 
-  def has_labels?(tags)
-    labels = ["bio", "organic", "fairtrade", "equitable"]
-    tags.any? { |label| labels.include?(label) }
+  def has_labels_bio?(tags)
+    labels = ["bio", "organic"]
+    tags.any? { |tag| labels.include?(tag.gsub("en:","").downcase) }
   end
 
+    def has_labels_fairtrade?(tags)
+    labels = ["fairtrade", "equitable"]
+    tags.any? { |tag| labels.include?(tag.gsub("en:","").downcase) }
+  end
+
+
   def is_french?(tags)
+    countries = ["france", "europe"]
     if tags.start_with?('[')
       JSON.parse(tags).any? { |label| label.downcase.include?("france") }
     else
       tags.downcase.include?("france")
     end
   end
-
 
    def has_plastic?(tags)
     if tags.start_with?('[')
@@ -110,8 +127,58 @@ class Product < ApplicationRecord
     end
   end
 
-  def total_score
-    social_score + environmental_score + health_score
+
+  def nutri_score(nutri)
+    if nutri.nil?
+      0
+    elsif nutri == "a"
+      50
+    elsif nutri == "b"
+      45
+    elsif nutri == "c"
+      30
+    elsif nutri == "d"
+      15
+    else
+      0
+    end
+  end
+
+  def nova_score(nova)
+     if nova.nil?
+      0
+    elsif nova == "1"
+      10
+    elsif nova == "2"
+      6
+    elsif nova == "3"
+      3
+    elsif nova == "4"
+      0
+    else
+      0
+    end
+  end
+
+  def additive_score(tags)
+  dangerous_additives = ["en:e102", "en:e110", "en:e123", "en:e124", "en:e127", "en:e131", "en:e142", "en:e154", "en:e160", "en:e163", "en:e154", "en:e102", "en:e110", "en:e120", "en:e123", "en:e124", "en:e125", "en:e126", "en:e120", "en:e173", "en:e175"]
+     if tags.count == 0
+      15
+    elsif tags.include?(dangerous_additives)
+      0
+    elsif tags.count <= 4
+      10
+    elsif tags.count <= 8
+      5
+    else
+      0
+    end
+  end
+
+
+  def total_score_compute
+    self.total_score = health_score + compute_environmental_score
+    self.save
   end
 
   def score_cap(score)
@@ -120,27 +187,19 @@ class Product < ApplicationRecord
     elsif score < -5
       -5
     else
-      scoreexi
+      score
     end
   end
 
   def self.recommendation(code)
     product = Product.find_by(code: code)
-    Product.select do |p|
-      p != product &&
-        p.pnns_group_2 == product.pnns_group_2 &&
-        p.health_score >= product.health_score
-    end.sort_by(&:total_score).first
+    product_cat = JSON.parse(product.categories_hierarchy).reverse.first
+
+    Product.
+      where.not(id: product.id).
+      where('categories_hierarchy like ?', "%#{product_cat}%").
+      where('total_score >= ?', product.total_score).
+      order(total_score: :desc).
+      first
   end
-
-  # def set_scores
-  #   self.health_score = pnns_second_group.health_score
-  #   self.environmental_score = ""
-  #   pnns_second_group.environmental_score
-  #   self.health_score = pnns_second_group.health_score
-  #   save!
-  # end
-
-  # def scoring
-  # end
 end
