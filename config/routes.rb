00c18188104e27c94d2761389781b1dd1bd315b9
Felix Letellier <felix.letellier.merida@gmail.com>
2019-09-04 09:39:44 +0200

    changes made to the scoring

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
   get "/lastbasket", to: "orders#last"
   get "/profile", to: "profiles#show"
   get "/dashboard", to: "profiles#dashboard"
+  get "/orders", to: "orders#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
