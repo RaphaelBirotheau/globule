0bd348bb995f68cfffd6a425fc04de09f1dfd3a4
thomas bozzo <thomas.bozzo@me.com>
2019-09-04 10:39:24 +0200

    footer debuged

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,21 @@
 class ProductsController < ApplicationController
 
-  def productlayout
-    render layout: "landing"
-  end
 
-  private
+  # private
 
-  def product_recommendation(code)
-    product = Product.find_by(code: code)
-    product_sub = Product.find_by('pnns_group_2 LIKE ? AND total_score >= ?', [product.pnns_group_2, product.total_score])
-  end
-
-  # def score
-  #   health_score = []
-  #   environment_score = []
-  #   social_score = []
-  #   total_score = []
-
-
+  # def product_recommendation(code)
+  #   product = Product.find_by(code: code)
+  #   product_sub = Product.find_by('pnns_group_2 LIKE ? AND total_score >= ?', [product.pnns_group_2, product.total_score])
   # end
+
+  # # def score
+  # #   health_score = []
+  # #   environment_score = []
+  # #   social_score = []
+  # #   total_score = []
+
+
+  # # end
 
 
 end
